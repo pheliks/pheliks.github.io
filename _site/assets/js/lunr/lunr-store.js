0716526d82a4477e10c9c9785f883a36d72630aa
Viktor Zamaraev <viktor.zamaraev@gmail.com>
2021-02-10 19:14:21 +0000

    Add DM and JCSS papers

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "8 Tools & Tips to Enhance Activity in an Online Classroom",
+        "excerpt":"Photo by Cookie the Pom on Unsplash Studies show that enhanced classroom experience can result in a better education and more sophisticated skill development. Researchers argue that classroom engagement can be enhanced for at least some students by simply introducing rather more active practices in the classroom. For me as...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2020/07/16/8-tools-and-tips-to-enhance-engagement-in-an-online-classroom.html",
+        "teaser": null
+      }]
