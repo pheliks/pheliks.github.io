7bffa7feffbc817d30134cb5460ad9561607e3b2
Viktor Zamaraev <viktor.zamaraev@gmail.com>
2021-02-10 19:17:58 +0000

    Add DM and JCSS papers 2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "8 Tools & Tips to Enhance Activity in an Online Classroom",
-        "excerpt":"Photo by Cookie the Pom on Unsplash Studies show that enhanced classroom experience can result in a better education and more sophisticated skill development. Researchers argue that classroom engagement can be enhanced for at least some students by simply introducing rather more active practices in the classroom. For me as...","categories": [],
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "http://localhost:4000/2020/07/16/8-tools-and-tips-to-enhance-engagement-in-an-online-classroom.html",
+        "url": "http://localhost:4000/jekyll/update/2020/02/18/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "8 Tools and Tips to Enhance Engagement in an Online Classroom",
+        "excerpt":"# Welcome  **Hello world**, this is my first Jekyll blog post.  I hope you like it!","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2020/07/16/8-tools-and-tips-to-enhance-engagement-in-an-online-classroom.MARKUP",
         "teaser": null
       }]
