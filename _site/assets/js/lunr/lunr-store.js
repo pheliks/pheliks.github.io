ef67e9ec2a057bed9e6c7464434aacfe38cbd1b4
Viktor Zamaraev <viktor.zamaraev@gmail.com>
2021-08-20 08:46:07 +0300

    Link update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "8 Tools & Tips to Enhance Activity in an Online Classroom",
+        "excerpt":"Photo by Cookie the Pom on Unsplash Studies show that enhanced classroom experience can result in a better education and more sophisticated skill development. Researchers argue that classroom engagement can be enhanced for at least some students by simply introducing rather more active practices in the classroom. For me as...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2020/07/16/8-tools-and-tips-to-enhance-engagement-in-an-online-classroom.html",
+        "teaser": null
+      },{
+        "title": "Enhancing students’ learning experience in the online environment",
+        "excerpt":"Photo by Bermix Studio on Unsplash This post is a condensed version of the detailed report of a study about efficiency of certain measures to enhance students' experience in the online environment. In the academic year 2020/2021, I was a module coordinator of the large module (250 students) “Data Mining...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2021/08/19/enhancing-students-learning-experience-in-the-online-environment.html",
+        "teaser": null
+      }]
