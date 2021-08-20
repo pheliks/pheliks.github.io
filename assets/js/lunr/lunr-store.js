9a30e0ee9e204a060af2071a2b3f6cc934014c2b
Viktor Zamaraev <viktor.zamaraev@gmail.com>
2021-08-20 08:31:19 +0300

    PGCAP paper on individual investigation

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/2020/07/16/8-tools-and-tips-to-enhance-engagement-in-an-online-classroom.html",
         "teaser": null
+      },{
+        "title": "Enhancing students’ learning experience in the online environment",
+        "excerpt":"Photo by Bermix Studio on Unsplash This post is a condensed version of the detailed report of a study about efficiency of certain measures to enhance students' experience in the online environment. In the academic year 2020/2021, I was a module coordinator of the large module (250 students) “Data Mining...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2021/08/19/enhancing-students-learning-experience-in-the-online-environment.html",
+        "teaser": null
       }]
